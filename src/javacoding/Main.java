9141955bce8d793f4ce3465b5ac2fc33d357018a
JinKyoungBin <skjin20800@naver.com>
2021-04-13 15:46:06 +0900

    11729 백준 - 하노이탑 [출력은 제대론대 왜 틀리지..]

--- src/javacoding/Main.java
+++ src/javacoding/Main.java
@@ -1,26 +1,46 @@
 package javacoding;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+
+class 재귀클래스{
+	// 흠 .. 잘 실행되는데 왜 틀렸는지 모르겠다 ㅠㅠ
+	int sum = 0;	
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	BufferedWriter sumResult = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public void 메인함수 (int result,int from, int to, int other) throws IOException {
+		this.sum= sum+1;
+		if(result == 1) {
+			bw.write(from+" "+to+"\n");
+			return ;
+			}		
+		메인함수(result-1,from,other,to);
+		bw.write(from+" "+to+"\n");
+		메인함수(result-1,other,to,from);
+	}		
+}
+
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-
-		  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	        StringTokenizer st = new StringTokenizer(reader.readLine());
-	        int a = Integer.parseInt(st.nextToken());
-	        int b = Integer.parseInt(st.nextToken());
-	        int c = Integer.parseInt(st.nextToken());
-	        if(b >= c) {
-	            System.out.println(-1);
-	        } else {
-	            System.out.println(a/(c-b)+1);
-	        }
-
-
-	
-			
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String brStr = br.readLine();
+		Integer brInt = Integer.parseInt(brStr);
+		재귀클래스 a = new 재귀클래스();
+			a.메인함수(brInt, 1, 3, 2);
+		
+		a.sumResult.write(a.sum+"\n");
+		a.sumResult.flush();
+		a.bw.flush();
+		a.sumResult.close();
+		a.bw.close();
+		
 		
 }
 }
